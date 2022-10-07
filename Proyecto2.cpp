414fd4cd34cd47715d2ab41f47c6111b718bfd49
GonzaloSantizo <84192252+GonzaloSantizo@users.noreply.github.com>
2022-10-07 07:17:36 -0600

    Changes made to base

--- Proyecto2.cpp
+++ Proyecto2.cpp
@@ -1,64 +1,63 @@
-/**
- * UNIVERSIDAD DEL VALLE DE GUATEMALA
- * 21504 - Gonzalo Santizo
- * Programacion de Microprocesadores
- * Seccion: 10
- * Proyecto 2
- */
+/*---------------------------------------------------------------------------
+* UNIVERSIDAD DEL VALLE DE GUATEMALA
+* FACULTAD DE INGENIERÍA
+* DEPARTAMENTO DE CIENCIA DE LA COMPUTACIÓN
+*
+* Curso:       CC3086 - Programación de microprocesadores     Ciclo II - 2022
+* Descripción: Encontrar el valor de convergencia de una serie
+* Creador Gonzalo Santizo - 21504
+------------------------------------------------------------------------------*/
 
-
-#include <pthread.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <iostream>
-#include <math.h>
-using namespace std;
+#include <pthread.h>
 
-//Primero iniciamos creando las variables 
-int n;
-double sumando = 0;
 
-//Luego se define la subrutina y su funcionamiento
-void *Sumatoria(void *numeros){
-    int number = (int)numeros;
-    double* resultado = new double;
-    *resultado = 3/(pow(2,number));
-    return resultado;
+void *SeriesValue(void *Value) //function that will fiund the value of a specific "n" number in a series
+{
+    int args = *(int *)Value;
+    *(double *)Value = (1.00000) / ((double)args * ((double)args + 1)); // 1 / (n(n+1))
+    return Value; // returns value
 }
-//Iniciamos con el main 
-int main(){
-    cout << "\n**************************************************************" <<endl;
-    cout << "Bienvenidos al programa 2 calculadora de sumatorias del proyecto 2 es un gusto atenderte" <<endl;
-    cout << "****************************************************************\n"<<endl;
-    cout << "Por favor, Ingresa el valor de n que deceas calcular: ->";
-    cin >> n;
-    cout << endl;
-    cout << "Este fue el valor de n que agregaste->: " << n << endl;
-    
-    //Aqui es para usar todo el proceso pthread y de los attr de la subrutina
-    int nums;
-    long i;
-    pthread_t tid;
-    pthread_attr_t attr;
-    pthread_attr_init(&attr);
-    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
 
-    //Se empieza creando el puntero para la base del resultado ingresado de la subrutina
-    void *suma;
-    //Aqui se inicia la sumatoria y la ira almacenando
-    for(int i = 0; i<=n ;i++){
-        nums = pthread_create(&tid, &attr, Sumatoria, (void *)i);
-        nums = pthread_join(tid, &suma);
-        sumando += (*(double*) suma);
+
+void *SeriesValue(void *Value);
+
+
+// driver code
+int main(int argc, char *argv[])
+{
+    int maxValue; //variable for user input
+    printf("enter max value of the series: ");
+    scanf("%d", &maxValue); // this records user input
+
+    pthread_t threads[maxValue]; // creates n amount of threads
+    int i;
+    for (i = 0; i < maxValue; i++)
+    {
+        int *parameter = (int *)malloc(sizeof(int)); //using malloc to ensure proper function
+        *parameter = i + 1;
+        if (pthread_create(&threads[i], NULL, &SeriesValue, parameter) != 0)
+        {
+            perror("Failed creating the threads");
+        }
     }
-    
-    cout << "******************************************************"<<endl;
-    cout << "Este es el resultado de la sumatoria->: " << sumando << endl;
-    cout << "******************************************************\n"<<endl;
-    cout << "Fue un gusto tenerte conmigo, Regresa pronto! <3 \n"<<endl;
-    pthread_exit(NULL);
+
+    double *ConvValue;
+    double sum = 0;
+    for (i = 0; i < maxValue; i++) // adds all threads together
+    {
+        if (pthread_join(threads[i], (void **)&ConvValue))
+        {
+            perror("Failing to join the threads to the main thread");
+        }
+        printf("\nEl Thread result %d, es %lf:", i + 1, *ConvValue);
+        sum += *ConvValue;
+        }
+    free(ConvValue);
+    printf("The final value of the series is: %lf\n", sum); //prints final result
     return 0;
 }
 
